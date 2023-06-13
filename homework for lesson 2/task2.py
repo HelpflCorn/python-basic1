b75c3246f82430ce4231575765157215b87a3f3d
HelpflCorn <ilyashaban@yahoo.com>
2023-06-13 13:53:01 +0200

    minor adjustments

--- homework for lesson 2/task2.py
+++ homework for lesson 2/task2.py
@@ -1,4 +1,4 @@
-print("hi there") #a regular print statement
-print("hi there", end="!") #the printed text ends with !
-print("hi there,", "what a beautiful day", end="!") #end= is adding "!" to the end of each string
-print("11", "06", "2023", sep=("-")) #sep= is adding a symbol between each of the strings that should be printed
+print("\nhi there") #a regular print statement
+print("\nhi there", end="!") #the printed text ends with !
+print("\nhi there,", "what a beautiful day", end="!") #end= is adding "!" to the end of each string
+print("\n11", "06", "2023", sep=("-")) #sep= is adding a symbol between each of the strings that should be printed
