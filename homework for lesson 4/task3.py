c0a2a35c8b3860813020180c53320c66f88058fa
HelpflCorn <ilyashaban@yahoo.com>
2023-06-15 15:39:09 +0200

    uploading task1

---
+++ homework for lesson 4/task3.py
@@ -0,0 +1,13 @@
+"""Task 3
+
+The math quiz program.
+
+Write a program that asks the answer for a mathematical expression, checks whether the user is right or wrong,
+and then responds with a message accordingly.
+
+ """
+s = input("What is the result of 9*20?:")
+if int(s) == 180:
+    print(True)
+else: print(False)
+
