d1eb14355920ba26e90944e2181a5ea4502c0d15
HelpflCorn <ilyashaban@yahoo.com>
2023-07-02 11:02:54 +0200

    commit

---
+++ homework for lesson 8/task1.py
@@ -0,0 +1,11 @@
+# Task 1
+
+# A simple function.
+
+# Create a simple function called favorite_movie, which takes a string containing the 
+# name of your favorite movie. The function should then print “My favorite movie is named {name}”.
+
+def favourite_movie(name):
+    print(f"my favourite movie is {name}")
+
+print(favourite_movie("Peppa Pig: My First Cinema Experience"))
